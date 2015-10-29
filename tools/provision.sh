8ee37b8a87bff55202e52a0f12d6da13f86191c0
Renato Souza <brt.renatos@gmail.com>
2015-10-29 19:25:31 -0200

    Fixes

--- tools/provision.sh
+++ tools/provision.sh
@@ -32,8 +32,6 @@ echo "[vagrant provisioning] Upgrade the packages for development..."
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 sudo add-apt-repository -y ppa:nginx/stable
 sudo add-apt-repository -y ppa:webupd8team/java
-sudo wget -qO - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key add -
-sudo echo "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
 sudo add-apt-repository -y ppa:git-core/ppa
 
 # Update packages
@@ -55,12 +53,6 @@ echo "[vagrant provisioning] Installing Java..."
 echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 sudo apt-get -y install oracle-jdk7-installer
 
-# Elasticsearch
-echo "[vagrant provisioning] Installing elasticsearch..."
-sudo apt-get -y install elasticsearch
-sudo /etc/init.d/elasticsearch start
-sudo update-rc.d elasticsearch defaults 95 10
-
 # Redis
 echo "[vagrant provisioning] Installing redis..."
 sudo apt-get -y install redis-server
@@ -76,10 +68,6 @@ sudo apt-get -y install vim
 # Packages
 echo "[vagrant provisioning] Installing additional packages..."
 sudo apt-get -y install gcc autoconf bison flex libtool make libboost-all-dev libcurl4-openssl-dev curl libevent-dev uuid-dev libhiredis-dev libmemcached-dev gperf build-essential
-
-# Additinal pakcages for development
-echo "[vagrant provisioning] Installing additional packages for development..."
-sudo apt-get -y install graphicsmagick libgraphicsmagick-dev imagemagick libghc-iconv-dev libmsgpack-dev
 
 # Create project directory
 echo "[vagrant provisioning] Creating /projects folder..."
